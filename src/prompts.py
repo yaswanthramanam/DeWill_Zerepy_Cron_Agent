d21e1990172f4c2946554da089853035ebbecf81
Noisy <125606576+donatik27@users.noreply.github.com>
2025-02-07 22:44:14 +0100

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -6,7 +6,7 @@ These templates are formatted strings that will be populated with dynamic data a
 #Twitter prompts
 POST_TWEET_PROMPT =  ("Generate an engaging tweet. Don't include any hashtags, links or emojis. Keep it under 280 characters."
                       "The tweets should be pure commentary, do not shill any coins or projects apart from {agent_name}. Do not repeat any of the"
-                      "tweets that were given as example. Avoid the words AI and crypto.")
+                      "tweets that were given as the examples. Avoid the words AI and crypto.")
 
 REPLY_TWEET_PROMPT = ("Generate a friendly, engaging reply to this tweet: {tweet_text}. Keep it under 280 characters. Don't include any usernames, hashtags, links or emojis. ")
 
